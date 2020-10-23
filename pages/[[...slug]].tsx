fd9b61c6acf523b38e3c6ef1b3bff577aa2bff0d
driquelmemurua <driquelmemurua@DESKTOP-I6V1P0B.localdomain>
2020-10-23 20:41:45 -0300

    log removed

--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -8,10 +8,6 @@ import { Main, Header, Footer } from 'components';
 import { HeaderProps } from 'types/components/Header';
 import { FooterProps } from 'types/components/Footer';
 
-import NEXT from 'next';
-
-if(process.env.NODE_ENV !== 'production') console.log(NEXT);
-
 export async function getStaticPaths() {
   return {
     paths: await PathsBuilder.build(),
